7180cfc2d013cf037dd4d561a62c66f3865c3d1d
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2020-11-30 15:45:51 -0300

    add todo model

---
+++ models/Todo.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        default: 'to do',
+        enum: ['to do', 'doing', 'done']
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Todo', TodoSchema);
