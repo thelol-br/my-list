325c62c3dc6b70bc6e6b5a02e1b85fc9b822855c
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2020-12-02 11:17:16 -0300

    update methodoverride put and delete

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const session = require('express-session');
 const passport = require('passport');
 const MongoStore = require('connect-mongo')(session);
@@ -24,6 +25,15 @@ const app = express();
 // Body parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Method Override
+app.use(methodOverride((req, res) => {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        let method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}))
 
 // Load Morgan logger
 if (process.env.NODE_ENV === 'development') {
